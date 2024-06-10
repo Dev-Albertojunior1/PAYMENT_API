5f5607c89d90949f978ac0a7aee9ee238af621f0
studentSystem <albertojunior848423803@gmail.com>
2024-06-10 15:40:51 -0700

    feat: recent Modifications

--- src/main/java/com/albertocode/PaymentAPI/domain/User/User.java
+++ src/main/java/com/albertocode/PaymentAPI/domain/User/User.java
@@ -1,5 +1,6 @@
 package com.albertocode.PaymentAPI.domain.User;
 
+import com.albertocode.PaymentAPI.DTOS.UserDTO;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -37,6 +38,14 @@ public class User {
     @Enumerated(EnumType.STRING)
     private UserType userType;
 
+    public  User(UserDTO data){
+        this.firstName=data.firstName();
+        this.lastName=data.lastName();
+        this.balance=data.balance();
+        this.userType=data.userType();
+        this.password=data.password();
+        this.email=data.email();
+    }
 
 
 }
