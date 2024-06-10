a860439b02ce82931812db4c216734f09b17f59b
studentSystem <albertojunior848423803@gmail.com>
2024-06-10 14:42:04 -0700

    feat: create Transaction entity

---
+++ src/main/java/com/albertocode/PaymentAPI/domain/Transaction/Transaction.java
@@ -0,0 +1,35 @@
+package com.albertocode.PaymentAPI.domain.Transaction;
+
+import com.albertocode.PaymentAPI.domain.User.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+
+}
