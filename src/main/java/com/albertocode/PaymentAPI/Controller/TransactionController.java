ac86b7801f1dcd8b9c3cc8865dd0d7db67b17bb3
studentSystem <albertojunior848423803@gmail.com>
2024-06-11 17:43:11 -0700

    feat: changes

---
+++ src/main/java/com/albertocode/PaymentAPI/Controller/TransactionController.java
@@ -0,0 +1,26 @@
+package com.albertocode.PaymentAPI.Controller;
+
+import com.albertocode.PaymentAPI.DTOS.TransactionDTO;
+import com.albertocode.PaymentAPI.Services.TransactionService;
+import com.albertocode.PaymentAPI.domain.Transaction.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transaction) throws Exception {
+        Transaction newTransaction = this.transactionService.createTransaction(transaction);
+        return new ResponseEntity<>(newTransaction, HttpStatus.OK);
+    }
+}
